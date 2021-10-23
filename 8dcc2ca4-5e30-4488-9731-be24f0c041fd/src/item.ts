4812009c812f32a1222b470137420361b6ebfe5c
tdokdak <86395324+tdokdak@users.noreply.github.com>
2021-10-23 20:57:27 +0700

    Add files via upload

---
+++ 8dcc2ca4-5e30-4488-9731-be24f0c041fd/src/item.ts
@@ -0,0 +1,64 @@
+export type Props = {
+  url: string
+  name?: string
+  bnw: boolean
+}
+
+let siteName = 'discord'
+let siteURL = 'discord.com'
+let defaulthover = 'Discord Page'
+
+let stringsToReplace: string[] = [
+  'http://',
+  'https://',
+  'http:',
+  'https:',
+  'www.',
+  siteURL,
+  '/',
+]
+
+export default class SMedia_Link implements IScript<Props> {
+  init() {}
+
+  spawn(host: Entity, props: Props, channel: IChannel) {
+    const link = new Entity()
+    link.setParent(host)
+
+    if (props.bnw) {
+      link.addComponent(new GLTFShape('8dcc2ca4-5e30-4488-9731-be24f0c041fd/models/discord_bnw.glb'))
+    } else {
+      link.addComponent(new GLTFShape('8dcc2ca4-5e30-4488-9731-be24f0c041fd/models/discord.glb'))
+    }
+
+    let url = parseURL(props.url)
+
+    let locationString = props.name ? props.name : defaulthover
+
+    link.addComponent(
+      new OnPointerDown(
+        async function () {
+          openExternalURL(url)
+        },
+        {
+          button: ActionButton.PRIMARY,
+          hoverText: locationString,
+        }
+      )
+    )
+  }
+}
+
+export function parseURL(url: string) {
+  let newURL = url.trim()
+
+  for (let str of stringsToReplace) {
+    if (newURL.substr(0, str.length) == str) {
+      newURL = newURL.substring(str.length).trim()
+    }
+  }
+
+  let finalURL = 'https://www.' + siteURL + '/' + newURL
+
+  return finalURL
+}
